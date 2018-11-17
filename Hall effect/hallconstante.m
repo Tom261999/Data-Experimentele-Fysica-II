5c29a4d68b50be2d7082e6b9b7fcb87f96545071
Tom <43322000+Tom261999@users.noreply.github.com>
2018-11-17 17:49:12 +0100

    Gemiddelde
    Voegt gemiddelde.m toe en berekend dit voor R in hallconstante.m

--- Hall effect/hallconstante.m
+++ Hall effect/hallconstante.m
@@ -11,17 +11,21 @@ Ib(find((Ib<=max(Idaal)) & (Ib>=min(Idaal))));
 Uh=UH(1:30)
  [Bb1,sBb1]=interpolatie2(Idaal,sIdaal,Bdaal,sBdaal,Ib,sIb)
  R=0.0001*Uh./(Bb1*0.020)
-% 
-% deletevalue = [3 4];
-% 
-% for i = length(deletevalue):-1:1
-%     R(deletevalue(i)) = [];
-%     Bb1(deletevalue(i)) = [];
-% end
-% 
-% 
-% UH
-% R
-% Bb1
-% figure(2)
-% plot(Bb1,R,'k.')
+
+deletevalue = [3 4];
+
+for i = length(deletevalue):-1:1
+    R(deletevalue(i)) = [];
+    Bb1(deletevalue(i)) = [];
+end
+
+
+UH
+R
+Bb1
+figure(2)
+plot(Bb1,R,'k.')
+
+disp('gemiddelde:')
+gemiddelde(R)
+disp('seems alright to me, voor ongedopeerd germanium is R_H = 0.012 m^3/C --Tom')
